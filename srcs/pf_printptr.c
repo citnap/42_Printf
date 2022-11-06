8c09ea1810942e058de7bf2634c4c1b1871f3061
Pakawat Panpaisan <ppanpais@z2t15c1.42bangkok.com>
2022-11-07 00:03:52 +0700

    editted some output for mac

--- srcs/pf_printptr.c
+++ srcs/pf_printptr.c
@@ -6,7 +6,7 @@
 /*   By: ppanpais <ppanpais@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 19:57:27 by ppanpais          #+#    #+#             */
-/*   Updated: 2022/10/14 15:44:59 by ppanpais         ###   ########.fr       */
+/*   Updated: 2022/11/06 22:24:17 by ppanpais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ unsigned int	pf_printptr(void *ptr)
 {
 	uintptr_t	n;
 
-	if (ptr == NULL)
-		return (pf_putstr("(nil)"));
 	n = (uintptr_t)ptr;
 	ft_putstr_fd("0x", 1);
 	return (2 + pf_print_shex(&n));
