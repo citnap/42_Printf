fcb00f4611462b505248bd1ac88282bad78e2155
Pakawat Panpaisan <ppanpais@z2t15c1.42bangkok.com>
2022-11-07 00:06:56 +0700

    fix norminete

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: ppanpais <ppanpais@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 19:57:18 by ppanpais          #+#    #+#             */
-/*   Updated: 2022/10/14 17:53:17 by ppanpais         ###   ########.fr       */
+/*   Updated: 2022/11/07 00:06:02 by ppanpais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include "libft/libft.h"
+
 unsigned int	pf_printptr(void *ptr);
 unsigned int	pf_print_bhex(unsigned long *n);
 unsigned int	pf_print_shex(unsigned long *n);
 unsigned int	pf_putstr(char *c);
-int		pf_print_data(t_list **data);
-int		ft_printf(const char *fmt, ...);
+int				pf_print_data(t_list **data);
+int				ft_printf(const char *fmt, ...);
 unsigned int	pf_putuint(unsigned int *n, int mode);
 unsigned int	pf_putchar(char *c);
 unsigned int	pf_putnbr(int *n);
-t_list		**pf_get_data(char *fmt, va_list arg);
-t_list		*pf_getdata_str(char *start, char *end);
-char		*pf_get_str(va_list arg);
+t_list			**pf_get_data(char *fmt, va_list arg);
+t_list			*pf_getdata_str(char *start, char *end);
+char			*pf_get_str(va_list arg);
 #endif
